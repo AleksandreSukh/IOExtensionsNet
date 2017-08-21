4ea4607a97bad79f293a836883c7a45355df1722
aleksandresukhitashvili@gmail.com <aleksandresukhitashvili@gmail.com>
2017-08-21 18:31:00 +0400

    Added some useful method to DirectoryExt

--- IOExtensionsNet/IOExtensionsNet/DirectoryExt.cs
+++ IOExtensionsNet/IOExtensionsNet/DirectoryExt.cs
@@ -10,6 +10,100 @@ namespace IOExtensionsNet
 {
     public static class DirectoryExt
     {
+        public static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs, bool tryOverwrite, bool tryRename)
+        {
+            // Get the subdirectories for the specified directory.
+            var dir = new DirectoryInfo(sourceDirName);
+            var dirs = dir.GetDirectories();
+
+            if (!Directory.Exists(dir.FullName))
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            // If the destination directory doesn't exist, create it. 
+            if (!Directory.Exists(destDirName))
+            {
+                Directory.CreateDirectory(destDirName);
+            }
+
+            // Get the files in the directory and copy them to the new location.
+            var files = dir.GetFiles();
+            foreach (var file in files)
+            {
+                var temppath = System.IO.Path.Combine(destDirName, file.Name);
+                FileExt.Copy(file.FullName, temppath, tryOverwrite, tryRename);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location. 
+            if (!copySubDirs) return;
+
+            foreach (var subdir in dirs)
+            {
+                var temppath = System.IO.Path.Combine(destDirName, subdir.Name);
+                DirectoryCopy(subdir.FullName, temppath, true, tryOverwrite, tryRename);
+            }
+        }
+
+        public static void DeleteDirectoryTree(string target_dir)
+        {
+            var files = Directory.GetFiles(target_dir);
+            var dirs = Directory.GetDirectories(target_dir);
+
+            foreach (var file in files)
+            {
+                File.SetAttributes(file, FileAttributes.Normal);
+                File.Delete(file);
+            }
+
+            foreach (var dir in dirs)
+            {
+                DeleteDirectoryTree(dir);
+            }
+
+            Directory.Delete(target_dir, false);
+        }
+
+        public static void CreateNewOrClear(string destDir)
+        {
+            if (Directory.Exists(destDir))
+            {
+                try
+                {
+                    Directory.Delete(destDir, true);
+                    Directory.CreateDirectory(destDir);
+                }
+                catch (IOException)
+                {
+                    foreach (var item in Directory.GetFiles(destDir))
+                    {
+                        File.Move(item, $"{item}.del.{Guid.NewGuid()}");
+                    }
+                }
+            }
+            else
+            {
+                Directory.CreateDirectory(destDir);
+            }
+        }
+
+        public static void CreateIfDoesNotExist(string destDir)
+        {
+            if (!Directory.Exists(destDir))
+            {
+                Directory.CreateDirectory(destDir);
+            }
+        }
+        public static string CreateForFile(string path)
+        {
+            var absolutePath = !Path.IsPathRooted(path) ? Path.Combine(Directory.GetCurrentDirectory(), path) : path;
+            var destDir = Path.GetDirectoryName(absolutePath);
+            CreateIfDoesNotExist(destDir);
+            return destDir;
+        }
+
         public static string CreateTempDir(string tempDirName)
         {
             var tempDirPath = Path.Combine(Path.GetTempPath(), tempDirName);
